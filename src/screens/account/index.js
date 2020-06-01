83962fb40fc64a17250b3724016589fc5e0f6ed4
Marcos-Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-01 14:51:18 -0700

    Create index.js
    conta, screen

---
+++ src/screens/account/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import 
+{ 
+    Container, 
+    Title,
+
+} from './styles'
+
+export default () => {
+    return(
+        <Container>
+            <Title>Home</Title>
+            
+        </Container>
+    )
+}
