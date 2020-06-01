b31c61a52e06b0115808d12d2bb4e6fc77d71d30
Marcos-Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-01 14:52:53 -0700

    Create styles.js

---
+++ src/screens/promo/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.SafeAreaView `
+    flex: 1;
+    background: #000;
+`;
+
+export const Title = styled.Text `
+    text-align: center;
+`;
