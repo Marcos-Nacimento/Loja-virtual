af09dcac4e7e1a2392b69f8cad042bdaac2bb270
Marcos-Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-01 14:52:17 -0700

    Create styles.js

---
+++ src/screens/home/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.SafeAreaView `
+    flex: 1;
+    background: #000;
+`;
+
+export const Title = styled.Text `
+    text-align: center;
+`;
+
+export const Header = styled.View `
+    justify-content: space-evenly;
+    flex-direction: row;
+`;
+
+export const Logo = styled.TouchableOpacity`
+    margin-top: 20px;
+`;
+
+export const Search = styled.TextInput `
+    margin-top: 20px;
+    font-size: 18px;
+    color: #fff;
+    font-weight: bold;
+    width: 200px;
+`;
+
+export const Description = styled.Text `
+    margin-top: 20px;
+    margin-left: 20px;
+    margin: 10px;
+    font-weight: bold;
+    font-size: 14px;
+    color: #fff;
+`;
+
+export const Content = styled.ScrollView ``;
+
+export const Load = styled.ActivityIndicator `
+    margin-top: 20px;
+`;
