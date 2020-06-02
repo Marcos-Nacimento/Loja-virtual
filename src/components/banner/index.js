628a882ac12faf2c99f10e28775782019f03183a
Marcos-Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-01 19:04:32 -0700

    Update index.js

---
+++ src/components/banner/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import 
+{ 
+    Content, 
+    Banner, 
+    Title, 
+    Description,
+
+} from './styles'
+
+export default () => {
+    return(
+        <Content>
+            <Banner>
+                <Description>
+                    <Title>Fique em casa !!</Title>
+                </Description>
+            </Banner>
+        </Content>
+    )
+}
