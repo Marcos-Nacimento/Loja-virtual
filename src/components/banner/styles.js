08dc41f89263f3fade69fb19184f618c7aa7da11
Marcos-Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-01 19:04:19 -0700

    Create styles.js

---
+++ src/components/banner/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native'
+
+export const Content = styled.View `
+    margin-top: 25px;
+    margin-bottom: 20px;
+`;
+
+export const Title = styled.Text `
+    margin: 11px;
+    font-weight: bold;
+    color: #fff;
+`;
+
+export const Banner = styled.View `
+    width: 100%;
+    height: 130px;
+    background: #7159c1;
+    border-radius: 10px;
+`;
+
+export const Description = styled.Text `
+    padding: 15px;
+    color: #fff;
+    font-weight: bold;
+    font-family: sans-serif;
+    font-size: 16px;
+`;
