484fae2cc24fa6c70b09c07f060bada3353a65d3
Marcos-Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-01 19:07:11 -0700

    Create index.js

---
+++ src/screens/settings/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container, Title } from './styles'
+
+export default () => {
+    return(
+        <Container>
+            <Title>settings</Title>
+        </Container>
+    )
+}
