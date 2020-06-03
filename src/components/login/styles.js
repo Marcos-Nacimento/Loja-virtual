653d96edf0a7011038b6241057a63ad77e63cb52
Marcos-Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-02 17:54:18 -0700

    Create styles.js
    screen login

---
+++ src/components/login/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View `
+    flex: 1;
+    background: #000;
+    align-items: center;
+`;
+
+export const Img = styled.Image `
+    margin: 100px;
+
+`;
+
+export const Input = styled.TextInput `
+    color: #fff;
+    margin: 5px;
+    width: 170px;
+    height: 25px;
+    font-size: 18px;
+`;
+
+export const Label = styled.Text `
+    text-align: center;
+    color: #fff;
+    font-weight: bold;
+`;
+
+export const Button = styled.TouchableOpacity `
+    margin: 10px;
+    background: #7159c1;
+    width: 170px;
+    height: 25px;
+    border-radius: 8px;
+    padding-left: 3px;
+`;
+
+export const Logos = styled.Image ``;
