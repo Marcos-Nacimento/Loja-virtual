32773fcd7a26f6f6196005213c90e46293fb814c
Marcos-Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-03 12:44:38 -0700

    Update index.js

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -15,18 +15,18 @@ const item =
 [
     {
         id: 1,
-        img: 'example',
-        descrip: 'name',
+        img: 'https://images.lojanike.com.br/1024x1024/produto/25889_1569664_20190801134152.png',
+        descrip: 'nike',
     },
     {
         id: 2,
-        img: 'example',
-        descrip: 'name',
+        img: 'https://images.lojanike.com.br/1024x1024/produto/25889_1569664_20190801134152.png',
+        descrip: 'nike',
     },
     {
         id: 3,
-        img: 'example',
-        descrip: 'name',
+        img: 'https://images.lojanike.com.br/1024x1024/produto/25889_1569664_20190801134152.png',
+        descrip: 'nike',
     }
 ]
 
@@ -38,7 +38,7 @@ export default () => {
                 {item.map((item) => (
                     <Option key={item.id}>
                         <Title>{item.descrip}</Title>
-                        <Img souce={item.img} />
+                        <Img style={{ width: 150, height: 200 }} souce={{ uri: item.img}} />
                     </Option>
                 ))}
             </Container>
