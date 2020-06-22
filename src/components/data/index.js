6655a1f2007bb5ab26aa235f7618bb64a0978e42
Marcos Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-22 06:25:26 -0300

    Update index.js

--- src/components/data/index.js
+++ src/components/data/index.js
@@ -12,7 +12,7 @@ export default
     },
     {
         id: String(Math.random()),
-        url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
+        url: 'https://i.pinimg.com/236x/e3/30/8c/e3308c49a5d31c82d7e92e99f01c09f3.jpg',
         name: 'camiseta',
     },
     {
