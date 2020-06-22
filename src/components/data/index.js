00b7f0ac6ae750888b28ca311914286f50967a7c
Marcos Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-22 05:49:54 -0300

    Create index.js

---
+++ src/components/data/index.js
@@ -0,0 +1,38 @@
+export default 
+[
+    {
+        id: String(Math.random()),
+        url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
+        name: 'camiseta',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
+        name: 'camiseta',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
+        name: 'camiseta',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
+        name: 'camiseta',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
+        name: 'camiseta',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
+        name: 'camiseta',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
+        name: 'camiseta',
+    },
+]
