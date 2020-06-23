2355878f092c2a783512b3df27db6149a3d1533a
Marcos Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-22 21:22:00 -0300

    Update index.js

--- src/components/data/index.js
+++ src/components/data/index.js
@@ -1,38 +1,77 @@
-export default 
+export const products =
 [
     {
         id: String(Math.random()),
         url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
-        name: 'camiseta',
+        value: '$ 79.90',
     },
     {
         id: String(Math.random()),
         url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
-        name: 'camiseta',
+        value: '$ 89.90',
     },
     {
         id: String(Math.random()),
         url: 'https://i.pinimg.com/236x/e3/30/8c/e3308c49a5d31c82d7e92e99f01c09f3.jpg',
-        name: 'camiseta',
+        value: '$ 120.90',
     },
     {
         id: String(Math.random()),
         url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
-        name: 'camiseta',
+        value: '$ 59.60',
     },
     {
         id: String(Math.random()),
         url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
-        name: 'camiseta',
+        value: '$ 89.99',
     },
     {
         id: String(Math.random()),
         url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
-        name: 'camiseta',
+        value: '$ 99.90',
     },
     {
         id: String(Math.random()),
         url: 'https://assets.xtechcommerce.com/uploads/images/medium/d5d252ecdefcb65b2c507898ef0f7038.jpg',
-        name: 'camiseta',
+        value: '$ 79.90',
+    },
+]
+
+export const Stories = 
+[
+    {
+        id: String(Math.random()),
+        url: 'https://m.media-amazon.com/images/G/32/Moda/2020/NewStoreFronts/Feminino/424_category-cards_roupas_434x530._SY530_QL85_.jpg',
+        name: 'pxp',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://ae01.alicdn.com/kf/H44fcebb9d1a4428a8c0caa59a9c9a9f5d/cheap-conjuntos-de-roupas-02.jpg',
+        name: 'pnb',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://ae01.alicdn.com/kf/H44fcebb9d1a4428a8c0caa59a9c9a9f5d/cheap-conjuntos-de-roupas-02.jpg',
+        name: 'pnb',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://ae01.alicdn.com/kf/H44fcebb9d1a4428a8c0caa59a9c9a9f5d/cheap-conjuntos-de-roupas-02.jpg',
+        name: 'pnb',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://ae01.alicdn.com/kf/H44fcebb9d1a4428a8c0caa59a9c9a9f5d/cheap-conjuntos-de-roupas-02.jpg',
+        name: 'pnb',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://ae01.alicdn.com/kf/H44fcebb9d1a4428a8c0caa59a9c9a9f5d/cheap-conjuntos-de-roupas-02.jpg',
+        name: 'pnb',
+    },
+    {
+        id: String(Math.random()),
+        url: 'https://ae01.alicdn.com/kf/H44fcebb9d1a4428a8c0caa59a9c9a9f5d/cheap-conjuntos-de-roupas-02.jpg',
+        name: 'pnb',
     },
 ]
